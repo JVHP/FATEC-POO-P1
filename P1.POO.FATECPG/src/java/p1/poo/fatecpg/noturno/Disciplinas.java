66fc348cea53f88b4ff9e690973d83012b8641c5
JVHP <63752069+JVHP@users.noreply.github.com>
2020-10-02 11:57:39 -0300

    EX 3 e 4

---
+++ P1.POO.FATECPG/src/java/p1/poo/fatecpg/noturno/Disciplinas.java
@@ -0,0 +1,61 @@
+package p1.poo.fatecpg.noturno;
+import java.util.ArrayList;
+
+public class Disciplinas {
+    ArrayList<Disciplinas> disciplinas = new ArrayList<>();
+    String nome;
+    String ementa;
+    int ciclo;
+    float nota;        
+
+    public Disciplinas(String nome, String ementa, int ciclo) {
+        this.nome = nome;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setList(ArrayList<Disciplinas> list) {
+        disciplinas = list;
+    }
+    
+    public void setterList(Disciplinas disc){
+        disciplinas.add(disc);
+    }
+    
+    public ArrayList<Disciplinas> getList() {
+        return disciplinas;
+    }
+
+}
---
+++ P1.POO.FATECPG/web/sobre.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Sobre</title>
+    </head>
+    <body>
+        <h1>Sobre</h1>
+        <p>Nome: José Vinícius Henrique Pereira</p>
+        <p>RA: 1290481913041</p>
+        <p>Repositório do GitHub: <a href="https://github.com/JVHP?tab=repositories">https://github.com/JVHP?tab=repositories</a></p>
+    </body>
+</html>
